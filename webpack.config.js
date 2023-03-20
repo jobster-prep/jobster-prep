702ab210f7bdaf0cbd5acad7da761fc986f92749
Jonah DH <jdh@jonahs-air.mynetworksettings.com>
2023-03-20 15:42:23 -0400

    set up redux skeleton (mostly)

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: ['./client/index.js'],
+  entry: ['./src/index.ts'],
   output: {
     path: path.join(__dirname, 'build'),
     publicPath: '/',
