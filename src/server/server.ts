4dda47c0a53cf4837d9fc98d725f3655dd3ced55
Jonah DH <jdh@jonahs-air.mynetworksettings.com>
2023-03-22 11:48:14 -0400

    commit before reverting

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,42 +1,31 @@
-import express, {
-    Request,
-    Response,
-    NextFunction,
-    RequestHandler,
-} from 'express';
+import express, {Request, Response, NextFunction, RequestHandler} from 'express';
 
 const app = express();
 app.use(express.json());
 
-app.get('/questions', (req: Request, res: Response, next: NextFunction) => 
-    res.sendStatus(200)
-);
+app.get('/questions', (req: Request, res: Response, next: NextFunction) => res.sendStatus(200));
 
-app.post('/questions', (req: Request, res: Response, next: NextFunction) => 
-    res.sendStatus(200)
-);
+app.post('/questions', (req: Request, res: Response, next: NextFunction) => res.sendStatus(200));
 
+console.log('hi from server');
 
 type ServerError = {
-    log: string;
-    status: number;
-    message: {
-        err: string;
-    };
+  log: string;
+  status: number;
+  message: {
+    err: string;
+  };
 };
 
-app.use(
-    '/',
-    (err: ServerError, req: Request, res: Response, next: NextFunction) => {
-      const defaultErr: ServerError = {
-        log: 'Express error handler caught unknown middleware error',
-        status: 400,
-        message: { err: 'An error occurred' },
-      };
-      const errorObj: ServerError = Object.assign({}, defaultErr, err);
-      console.log(errorObj.log);
-      return res.status(errorObj.status).json(errorObj.message);
-    }
-  );
-  
-  app.listen(3000, () => console.log('server is listening on port 3000'));
+app.use('/', (err: ServerError, req: Request, res: Response, next: NextFunction) => {
+  const defaultErr: ServerError = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: {err: 'An error occurred'},
+  };
+  const errorObj: ServerError = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(3000, () => console.log('server is listening on port 3000'));
